8586f6c77f87e67e01f44610867933013af9feab
Luke Dunn <lukedunn011@gmail.com>
2023-02-08 01:22:19 +1100

    woo
    weeoo

--- authen/signin/main.js
+++ authen/signin/main.js
@@ -3,6 +3,8 @@ const url = "http://localhost:3000/login";
 const form = document.getElementById("login-form");
 let username = document.getElementById("username");
 let password = document.getElementById("password");
+let error = document.getElementById("error-message"); 
+
 
 form.addEventListener("submit", function (event) {
   console.log("Form submitted"); 
@@ -14,16 +16,19 @@ let data = {
       };  
   
 
-console.log(data); 
+
 
 axios.post(url, data)
   .then(response => { 
     if (response.data === "found") {
-        
       console.log("success"); 
       sessionStorage.setItem("username", username.value); 
       window.location.href = "/authen/landingpage/landingpage.html";
         }
+      else {
+        error.innerHTML += "Invalid username or password"; 
+        error.style.color = "red";
+       }
       })
   .catch(error => {
       console.log(error);
--- authen/signup/signup.js
+++ authen/signup/signup.js
@@ -1,32 +1,32 @@
 const url = "http://localhost:3000/signup"; 
-
 const form = document.getElementById("signup-form");
 let username = document.getElementById("username");
 let password = document.getElementById("password");
 let confirmPassword = document.getElementById("confirm-password"); 
 let errorMessage = document.getElementById("error-message");
+
+
 form.addEventListener("submit", function (event) { 
   console.log("Form submitted"); 
   event.preventDefault();   
+
 
 let data = { 
     username: username.value,
     password: password.value,
 } 
 
+
 if (password.value === confirmPassword.value) {
-    console.log(data); 
     signup(data); 
 } else { 
     errorMessage.innerHTML = "Passwords do not match";
     errorMessage.style.color = "red"; 
-    console.log("Passwords do not match"); 
     } 
 }); 
 
 
 function signup(data) {
-
     axios.put(url, data) 
     .then(response => {
         if (response === "added") { 
@@ -37,5 +37,4 @@ function signup(data) {
     .catch(error => { 
         console.log(error); 
     }); 
-
 }
